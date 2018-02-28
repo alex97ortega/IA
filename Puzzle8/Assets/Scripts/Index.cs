c9c8d53e9416c26364b5be696e63422ff8a91a68
alex97ortega <alejor01@ucm.es>
2018-02-28 12:08:40 +0100

    a toma po culo BEtter_Organitation

---
+++ Puzzle8/Assets/Scripts/Index.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Index : MonoBehaviour {
+    public uint index;
+    public uint GetIndex() { return index; }
+}
