c2b497aaed0be4d4be23ad18ccb112848bd748d5
Borja Cano <borcano@ucm.es>
2018-03-01 20:16:47 +0100

    Seguimos sin hacer que LA PUTA MIERDA ÉSTA fnucione

--- Puzzle8/Assets/Scripts/Hill_Climbing.cs
+++ Puzzle8/Assets/Scripts/Hill_Climbing.cs
@@ -13,10 +13,11 @@ public class Hill_Climbing : MonoBehaviour
     int[,] solvedState = new int[3, 3];
     struct Node
     {
-        public int[,] state;
-        public int fichaMovida;
-        public Vector2Int huecoPos;
+        public int[,] state;//almacena el tablero
+        public int fichaMovida;//el índice de la pieza que seha movido en esa jugada
+        public Vector2Int huecoPos;//la posición del hueco en esa jugada
 
+        //La constructora simplemente busca el hueco y copia el stado por parámetro
         public Node(int[,] state_)
         {
             state = state_;
@@ -29,8 +30,9 @@ public class Hill_Climbing : MonoBehaviour
                         huecoPos = new Vector2Int(i, j);
         }
     }
-
+    //lista de los estados candidatos
     List<Node> list = new List<Node>();
+    //lista de estados por los que ya hemos pasado
     List<int[,]> seen = new List<int[,]>();
 
     Vector2Int[] directions;
@@ -53,49 +55,45 @@ public class Hill_Climbing : MonoBehaviour
     }
 
     public void Resolve()
-    { 
+    { //Metemos a la lista de candidatos el estado actual del juego
         list.Add(new Node(TranslateCurrentState(fichas)));
-        Debug.Log("Tablero Original:");
-        PrintStado(list[0].state);
+        //PrintStado(list[0].state, "Tablero Original:");
 
-        int cont = 0;
-        bool nuevoEstado;
+        int cont = 1;//Debug
+        //Mientras no encontremos la solucion, seguir buscando
         while (list.Count > 0 && !IsSolution(list[0].state))
         {
-            Debug.Log("Vuelta :" + cont);
-            nuevoEstado = false;
+            Node currentState = list[0];
+            Debug.Log("--------------------------------");
+            Debug.Log("Vuelta: " + cont + " el tamaño de la lista es:" + list.Count);
             //Probar todos los movimientos posibles
-            for (int i = 0; i < 4; i++)
+            foreach (Vector2Int dir in directions)
             {
-                if (!nuevoEstado)
+                if (IsValidMove(currentState, dir)) // is valid es correcto
                 {
-                    if (IsValidMove(list[0], directions[i])) // is valid es correcto
+                    PrintStado(currentState.state, "Estado original");
+                    //El problema esta en el mueve ficha, el estado se le pasa correctamente
+                    //Pero lo devuelve mal, como si recordase el tabelro anterior dentro del método
+                    Node newNode = MueveFicha(currentState, dir);
+                    // Debug.Log("El hueco está en :" + newNode.huecoPos);
+                    //Debug.Log("Muevo la ficha: "+ (newNode.fichaMovida + 1));
+                    //descartar los estados por los que ya hemos pasado ( !seen.Constains(state)  )
+                    PrintStado(newNode.state, "Estado tras mover Ficha");
+                    bool contenido = seen.Contains(newNode.state);
+                    if (!contenido)
                     {
-                       
-                        Node newNode = MueveFicha(list[0], directions[i]);
-                        // Debug.Log("El hueco está en :" + newNode.huecoPos);
-                        //Debug.Log("Muevo la ficha: "+ (newNode.fichaMovida + 1));
-                        //eliminar los estados por los que ya hemos pasado ( !seen.Constains(state)  )
-                        bool contenido = seen.Contains(newNode.state);
-                        if (!contenido)
-                        {
-                            //meter los nuevos estados en la lista de estados
-                            Debug.Log("Estado Nuevo");
-                            list.Add(newNode);
-                            PrintStado(newNode.state);
-                            Debug.Log("Tamaño de la lista: " + list.Count);
-                            nuevoEstado = true;
-                        }
-                        else Debug.Log("Estado ya visto"); // en la segunda vuelta solo entra aqui
+                        //meter los nuevos estados en la lista de estados
+                       // Debug.Log("Estado nuevo");
+                        list.Add(newNode);
                     }
+                   // else Debug.Log("Estado ya visto"); // en la segunda vuelta solo entra aqui
                 }
-                
             }
             //quitar list[0] y meterlo en seen
-            seen.Add(list[0].state);
-            list.Remove(list[0]);
+            seen.Add(currentState.state);
+            list.Remove(currentState);
             //ordenar la lista de acuerdo a la heurística
-            Debug.Log("Tamaño de la lista final: " + list.Count);
+          //  Debug.Log("Tamaño de la lista final: " + list.Count);
             SortByPotential(ref list);
             cont++;
         }//Solver Algorithm
@@ -113,7 +111,7 @@ public class Hill_Climbing : MonoBehaviour
         //return solution;
     }
 
-    //Done
+    //Comprueba si el estado que se le pasa es el resuelto
     bool IsSolution(int[,] state)
     {
         bool ret = true;
@@ -124,7 +122,7 @@ public class Hill_Climbing : MonoBehaviour
         return ret;
     }
 
-    //Done
+    //Taduce el array de GameObjects donde están las fichas en un estado de int[,]
     int[,] TranslateCurrentState(GameObject[] fichas)
     {
         //Inicializamos el estado a -1
@@ -141,29 +139,32 @@ public class Hill_Climbing : MonoBehaviour
             currentState[pos.y, pos.x] = index;
             //Debug.Log("La ficha " + index + " está en la posicion: " + pos);
         }
-
-
-
         //Devolvemos el estado formateado
-       /* Debug.Log("Tablero traducido a:\n"
-            + currentState[0, 0] + " " + currentState[0, 1] + " " + currentState[0, 2] + "\n"
-            + currentState[1, 0] + " " + currentState[1, 1] + " " + currentState[1, 2] + "\n" 
-            + currentState[2, 0] + " " + currentState[2, 1] + "  " + currentState[2, 2]);*/
         return currentState;
     }
 
+    //Devuelve el nuevo estado resultado de mover la ficha en la dirección que se le pasa
     Node MueveFicha(Node oldState, Vector2Int dir)
     {
+        //copiamos el estado del que venimos
+        PrintStado(oldState.state, "Old state recibido en Mueve Ficha");
         int[,] newState = oldState.state;
-
+        //calculamos la posición del huevo en el nuevo estado
         Vector2Int newHuecoPos = oldState.huecoPos + dir;
+        //Creamos un nuevo Nodo, inicializando la ficha movida al valor que tenía 
+        //la ficha del estado anterior en la posición en la que ahora está el hueco
+        //porque se han intercambiado
         Node newNode = new Node
         {
             fichaMovida = oldState.state[newHuecoPos.x, newHuecoPos.y]
         };
+        //Ponemos en la posicion del hueco antiguo el valor de la casilla que ahora va a ser el hueco
+        Debug.Log("He movido la ficha: " + newNode.fichaMovida);
         newState[oldState.huecoPos.x, oldState.huecoPos.y] = oldState.state[newHuecoPos.x, newHuecoPos.y];
+        //Ponemos el hueco a -1 para representarlo
+        Debug.Log(message: "Valor del nuevo hueco antes de ponerlo a -1 " +newState[newHuecoPos.x, newHuecoPos.y]);
         newState[newHuecoPos.x, newHuecoPos.y] = -1;
-
+        //Inicializamos los valores del nuevo Node con la información calculada
         newNode.huecoPos = newHuecoPos;
         newNode.state = newState;
 
@@ -171,15 +172,19 @@ public class Hill_Climbing : MonoBehaviour
     }
     bool IsValidMove(Node n, Vector2Int dir)
     {
+        //Calculamos la posición del hueco si se moviese en la direccion que se pasa por parámetro
+        //devolvemos si la posición resultante es válida
         Vector2Int newPos = n.huecoPos + dir;
         bool resultado = !(newPos.x > 2 || newPos.x < 0 || newPos.y > 2 || newPos.y < 0);
         return resultado;
     }
 
-
+    //Estructura para poder darle un valor a cada nodo
     struct WeightedNode { public int weigth; public Node node; }
+    //Reordenamos la lista de candidatos para que el más prometedor sea el primero
     void SortByPotential(ref List<Node> list)
     {
+        //Lista con los penos
         List<WeightedNode> wList = new List<WeightedNode>();
         foreach (Node n in list)
         {
@@ -189,14 +194,21 @@ public class Hill_Climbing : MonoBehaviour
             wList.Add(weightedNode);
         }
         //Sort wList
-        wList.Sort((x, y) => x.weigth.CompareTo(y.weigth));
+        //de menor a mayor (<)
+        wList.Sort((x, y) => {
+            if (x.weigth >= y.weigth)
+                return -1;
+            else
+                return 1;
+            });
         //Copy WList.node -> list
         for (int i = 0; i < list.Count; i++)
         {
             list[i] = wList[i].node;
         }
     }
-
+    //Devuelve la distancia de la última ficha movida en ese estado 
+    //hacia la que debería ser su posición
     int GetWeight(Node node)
     {
         //Contar la distancia entre la pieza movida y su posición correcta
@@ -219,10 +231,10 @@ public class Hill_Climbing : MonoBehaviour
         int distancia = (fichaMovidaPos.x - posDestino.x) + (fichaMovidaPos.y - posDestino.y); ;
         return distancia;
     }
-
-    static void PrintStado(int[,] state)
+    //Esto simplemente imprime el tablero en la consola debug de Unity
+    static void PrintStado(int[,] state, string text)
     {
-        Debug.Log("Tablero:\n"
+        Debug.Log(text + "\n"
            + state[0, 0] + " " + state[0, 1] + " " + state[0, 2] + "\n"
            + state[1, 0] + " " + state[1, 1] + " " + state[1, 2] + "\n"
            + state[2, 0] + " " + state[2, 1] + "  " + state[2, 2]);
