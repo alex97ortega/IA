cfa36bc8db27179fdc619a6fc8ae3dd92bb7a8fd
Usuario Local <alejor01@ucm.es>
2018-05-11 10:30:41 +0200

    empezada la p5

---
+++ 5-Fantasmas/IAVP5-BD-TPC/Assets/Third Person Controller/Scripts/MouseDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseDetection : MonoBehaviour {
+
+    
+    public GameObject npc;
+    void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            npc.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+        }
+    }
+}
