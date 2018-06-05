17446c64f5c655d29e8a9ca45f2290fdef2589bc
alex97ortega <alejor01@ucm.es>
2018-06-05 17:37:23 +0200

    guapisimo

---
+++ 5-Fantasmas/IAVP5-BD-TPC/Assets/CollisionGhost.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Opsive.ThirdPersonController.Wrappers;
+
+public class CollisionGhost : MonoBehaviour {
+
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 21)
+        {
+            Debug.Log("cosas");
+            GetComponent<CharacterHealth>().Damage(2, transform.position, new Vector3(0, 0, 0));
+        }
+    }
+}
